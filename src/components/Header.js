bd87a3750e17e26de8c9f99d9967f2e3a2ff065d
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-14 21:59:28 +0200

    Contador de senyores estático

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,24 @@ export const Header = (props) => {
   const cambiarMarcado = () => {
     marcado2 ? setMarcado2(false) : setMarcado2(true);
   };
+  const contador = () => {
+    const contadorMarcados = arraySenyores.filter(
+      (senyor) => senyor.marcado
+    ).length;
+    return contadorMarcados;
+  };
   return (
     <header className="cabecera text-center py-2 row">
       <h1 className="col-12">Señores que te apuntan con el dedo</h1>
       <span className="col-12 mt-2">
-        <span className="total">No he podido calcular cuántos</span> señores que
-        te apuntan con el dedo hay marcados
+        <span className="total">{contador()}</span> señores que te apuntan con
+        el dedo marcados
       </span>
       <button
         className="marcar-todos col-2 offset-5 btn mt-2"
         onClick={cambiarMarcado}
       >
-        No he podido hacer funcionar este botón
+        Marcar todos
       </button>
     </header>
   );
